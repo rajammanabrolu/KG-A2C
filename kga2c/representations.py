e963cf8a7e2e061a24674ada0a63ffc8b5222f14
Matthew Hausknecht <matthew.hausknecht@gmail.com>
2019-12-30 14:11:51 -0800

    cleanup.

--- kga2c/representations.py
+++ kga2c/representations.py
@@ -14,7 +14,6 @@ import logging
 from fuzzywuzzy import fuzz
 
 from jericho.util import clean
-from text_agents.common.util import pad_sequences
 
 import redis
 
@@ -377,3 +376,43 @@ class StateAction(object):
         self.graph_state_rep = self.get_state_rep_kge(), self.adj_matrix
 
         return ret, ret_cache
+
+
+def pad_sequences(sequences, maxlen=None, dtype='int32', value=0.):
+    '''
+    Partially borrowed from Keras
+    # Arguments
+        sequences: list of lists where each element is a sequence
+        maxlen: int, maximum length
+        dtype: type to cast the resulting sequence.
+        value: float, value to pad the sequences to the desired value.
+    # Returns
+        x: numpy array with dimensions (number_of_sequences, maxlen)
+    '''
+    lengths = [len(s) for s in sequences]
+    nb_samples = len(sequences)
+    if maxlen is None:
+        maxlen = np.max(lengths)
+    # take the sample shape from the first non empty sequence
+    # checking for consistency in the main loop below.
+    sample_shape = tuple()
+    for s in sequences:
+        if len(s) > 0:
+            sample_shape = np.asarray(s).shape[1:]
+            break
+    x = (np.ones((nb_samples, maxlen) + sample_shape) * value).astype(dtype)
+    for idx, s in enumerate(sequences):
+        if len(s) == 0:
+            continue  # empty list was found
+        # pre truncating
+        trunc = s[-maxlen:]
+        # check `trunc` has expected shape
+        trunc = np.asarray(trunc, dtype=dtype)
+        if trunc.shape[1:] != sample_shape:
+            raise ValueError('Shape of sample %s of sequence at position %s is different from expected shape %s' %
+                             (trunc.shape[1:], idx, sample_shape))
+        # post padding
+        x[idx, :len(trunc)] = trunc
+    return x
+
+
